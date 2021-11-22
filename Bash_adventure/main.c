a57d9cebc21befafbcc9cc39d3bf154d380c1e30
vroum-vroum <92180616+vroum-vroum@users.noreply.github.com>
2021-11-22 16:12:00 -0400

    ajout menu

--- Bash_adventure/main.c
+++ Bash_adventure/main.c
@@ -5,8 +5,35 @@
 
 int main()
 {
+    int choix;
     Game *partie;
-    initGame(&partie);
+    printf("bash adventure\n\n bienvenue\n\n");
+
+    printf("--menu--\n");
+    printf("1-jouer\n");
+    printf("2charger une sauvegarde\n");
+    printf("3-supprimer une sauvegarde\n");
+    printf("4-quitter\n");
+    scanf("%d",&choix);
+    system("cls");
+    switch(choix)
+    {
+    case 1:
+        initGame(&partie);
+        break;
+    case 2:
+        //fonction charger supp sauvegarde
+        break;
+    case 3:
+        //fonction pour explique comment on joue
+        break;
+    case 4:
+        break;
+    default:
+        printf("choisi correctement\n");
+        break;
+    }
+
 
     return 0;
 }
